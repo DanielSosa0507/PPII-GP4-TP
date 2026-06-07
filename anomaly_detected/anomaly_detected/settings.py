35c97cc76bd71318c3bf6860585f0b013c66505f
SandritaLopez <sandra.lopez.code@gmail.com>
2026-06-07 15:59:57 -0300

    Serve frontend index.html at root URL

--- anomaly_detected/anomaly_detected/settings.py
+++ anomaly_detected/anomaly_detected/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = "anomaly_detected.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR.parent / 'frontend'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
