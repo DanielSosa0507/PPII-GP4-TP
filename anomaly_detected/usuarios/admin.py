8c554b1488754546062759d79f87ee92f04f29b1
Stephany Rodriguez <scrodriguezcapote@gmail.com>
2026-06-21 13:54:42 -0300

    WIP: guardo cambios antes de borrar rama

--- anomaly_detected/usuarios/admin.py
+++ anomaly_detected/usuarios/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
-admin.site.register(Usuario)
+
+class UsuarioAdmin(UserAdmin):
+    list_display = ('username', 'email', 'rol', 'nivel', 'is_staff', 'is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Perfil Anomaly Detected', {
+            'fields': (
+                'rol', 'foto_perfil', 'bio',
+                'nombre', 'apellido', 'nivel',
+                'lugares_visitados', 'estados_explorados', 'fenomenos_comentados',
+            )
+        }),
+    )
+
+
+admin.site.register(Usuario, UsuarioAdmin)
